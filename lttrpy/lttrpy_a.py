9ada4d266eab42bf9c6668e8676771cc662a9049
Said Sattarov <gallifreyan@protonmail.ch>
2024-02-19 03:10:49 +0300

    finally finished that parsing thing

--- lttrpy/lttrpy_a.py
+++ lttrpy/lttrpy_a.py
@@ -15,14 +15,16 @@
 # TODO: a prettier table?
 
 import trio
+
 # h2 needs to be present because we're making extensive use of it here
 from httpx import AsyncClient
 from lxml import html
 import sys
 
 
-assert sys.version_info[0] >= 3 and sys.version_info[1] >= 9, \
-    "This script requires Python 3.9 or newer to run. Exiting."
+assert (
+    sys.version_info[0] >= 3 and sys.version_info[1] >= 9
+), "This script requires Python 3.9 or newer to run. Exiting."
 
 
 async def get_page(session, url):
@@ -35,12 +37,7 @@ async def get_page(session, url):
 
 class LetterboxdFilm:
     # will work on this later
-    def __init__(self,
-                 film_id,
-                 session=None,
-                 ratings=[],
-                 reviews=[],
-                 liked=set()):
+    def __init__(self, film_id, session=None, ratings=[], reviews=[], liked=set()):
 
         self.string_id = film_id
         self.film_page = f"https://letterboxd.com/film/{self.string_id}"
@@ -68,19 +65,32 @@ class LetterboxdFilm:
             print(f"{self.title} ({self.year}) watched by {user}")
 
     async def get_review(self, username):
-        page = await get_page(self.session, 
-                              f'https://letterboxd.com/{username}/film/{self.string_id}')
-        tree = html.document_fromstring(page.text)
-        review = tree.xpath("/html/head/meta[4]")[0].get('content')
+        REVIEW_PAGE = "https://letterboxd.com/{}/film/{}/"
+        page = (
+            await self.session.get(REVIEW_PAGE.format(username, self.string_id))
+        ).text
+        tree = html.document_fromstring(page)
+        spoiler = (
+            True
+            if (
+                "This review may contain spoilers"
+                in tree.xpath("//meta[@name='description'][1]")[0].get("content")
+            )
+            else None
+        )
+        review = "\n".join(
+            tree.xpath(
+                "/html/body/div[1]/div/div/section/section/div[1]/div/div/p/text()"
+            )
+        )
+        return spoiler, review
 
     def update(self, username):
         pass
 
 
 class LetterboxdProfile:
-    def __init__(self,
-                 username,
-                 session):
+    def __init__(self, username, session):
 
         self.username = username
         self.link = f"https://letterboxd.com/{self.username}"
@@ -102,14 +112,53 @@ class LetterboxdProfile:
     def __contains__(self, item):
         return item in self.films_watched
 
-    def get_page(self, pagenum):
+    async def get_review(session, username, film):
+        REVIEW_PAGE = "https://letterboxd.com/{}/film/{}/"
+        page = (await session.get(REVIEW_PAGE.format(username, film))).text
+        tree = html.document_fromstring(page)
+        spoiler = (
+            True
+            if (
+                "This review may contain spoilers"
+                in tree.xpath("//meta[@name='description'][1]")[0].get("content")
+            )
+            else None
+        )
+        review = "\n".join(
+            tree.xpath(
+                "/html/body/div[1]/div/div/section/section/div[1]/div/div/p/text()"
+            )
+        )
+        return spoiler, review
+
+    def find_films(page):
+        films = {
+            node.xpath("./div")[0].get("data-film-slug"): {
+                "html": node,
+                "title": node.xpath("./div[1]/img")[0].get("alt"),
+                "rating": node.xpath("./p/span[1]/text()"),
+                "liked": True if node.xpath("./p/span[2]") else False,
+                "reviewed": True if node.xpath("./p/a") else False,
+            }
+            for node in page.xpath("//ul/li[@class='poster-container']")
+        }
+        return films
+
+    async def get_all_pages(session, username):
+        page1 = await get_page(session, username, 1)
+        last_page = int(page1.xpath("//li[@class='paginate-page'][3]/a/text()")[0])
+        return [page1] + [
+            (await get_page(session, username, page))
+            for page in range(2, last_page + 1)
+        ]
+
+    async def get_page(session, username, pagenum):
         LIST_PAGE = "https://letterboxd.com/{}/films/page/{}"
-        return await self.session.get(LIST_PAGE.format(self.username, pagenum)).text()
+        page = (await session.get(LIST_PAGE.format(username, pagenum))).text
+        return html.document_fromstring(page)
 
-    # def 
 
-
-def main():
+async def main():
     async with AsyncClient(http2=True, follow_redirects=True) as client:
         ALL_FILMS = {}
         users = (user1, user2)
